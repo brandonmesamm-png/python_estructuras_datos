ce8f93ad666bdbfbe2cec3d8e4d8d966f6a12beb
brandon <brandonmesamm@gmail.com>
2026-07-28 20:13:10 -0500

    Primer commit: actividad 3 estructuras de datos en Python

---
+++ modulo2_tuplas/reto_tuplas.py
@@ -0,0 +1,4 @@
+coordenadas = (5, 8)
+
+print("X:", coordenadas[0])
+print("Y:", coordenadas[1])
---
+++ modulo3_diccionarios/reto_diccionarios.py
@@ -0,0 +1,8 @@
+producto = {
+    "nombre": "Teclado",
+    "precio": 85000,
+    "cantidad": 3
+}
+
+for clave, valor in producto.items():
+    print(clave, ":", valor)
